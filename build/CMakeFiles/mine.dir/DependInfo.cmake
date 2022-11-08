a25d12c8707d1b12f51bfe795e3d8774a6a2aac7
Phw9 <hyunwoo1417@hanyang.ac.kr>
2022-11-08 20:15:40 +0900

    temp

--- build/CMakeFiles/mine.dir/DependInfo.cmake
+++ build/CMakeFiles/mine.dir/DependInfo.cmake
@@ -4,10 +4,10 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "/home/phw/Dev/MVO/src/Feature.cpp" "/home/phw/Dev/MVO/build/CMakeFiles/mine.dir/src/Feature.cpp.o"
-  "/home/phw/Dev/MVO/src/Init.cpp" "/home/phw/Dev/MVO/build/CMakeFiles/mine.dir/src/Init.cpp.o"
-  "/home/phw/Dev/MVO/src/PoseEstimation.cpp" "/home/phw/Dev/MVO/build/CMakeFiles/mine.dir/src/PoseEstimation.cpp.o"
-  "/home/phw/Dev/MVO/src/Triangulate.cpp" "/home/phw/Dev/MVO/build/CMakeFiles/mine.dir/src/Triangulate.cpp.o"
+  "/home/phw9/MVO/src/Feature.cpp" "/home/phw9/MVO/build/CMakeFiles/mine.dir/src/Feature.cpp.o"
+  "/home/phw9/MVO/src/Init.cpp" "/home/phw9/MVO/build/CMakeFiles/mine.dir/src/Init.cpp.o"
+  "/home/phw9/MVO/src/PoseEstimation.cpp" "/home/phw9/MVO/build/CMakeFiles/mine.dir/src/PoseEstimation.cpp.o"
+  "/home/phw9/MVO/src/Triangulate.cpp" "/home/phw9/MVO/build/CMakeFiles/mine.dir/src/Triangulate.cpp.o"
   )
 set(CMAKE_CXX_COMPILER_ID "GNU")
 
@@ -15,16 +15,16 @@ set(CMAKE_CXX_COMPILER_ID "GNU")
 set(CMAKE_TARGET_DEFINITIONS_CXX
   "HAVE_EIGEN"
   "HAVE_GLEW"
-  "PANGO_DEFAULT_WIN_URI=\"wayland\""
+  "PANGO_DEFAULT_WIN_URI=\"x11\""
   "_LINUX_"
+  "mine_EXPORTS"
   )
 
 # The include file search paths:
 set(CMAKE_CXX_TARGET_INCLUDE_PATH
-  "/usr/include/eigen3"
-  "/usr/local/include"
-  "/usr/local/include/opencv"
   "../include"
+  "/usr/include/eigen3"
+  "/usr/local/include/opencv"
   )
 
 # Targets to which this target links.
